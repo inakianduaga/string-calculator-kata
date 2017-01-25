4b2ab72f6a2e4016eeccf83d40784f313dc835fa
Inaki Anduaga <inaki.anduaga@tado.com>
2017-01-25 01:49:26 +0100

    Trying to wrap negative exception check into fluent chain, not working

--- src/main/scala/com/inakianduaga/StringCalculator.scala
+++ src/main/scala/com/inakianduaga/StringCalculator.scala
@@ -1,16 +1,20 @@
 package com.inakianduaga
-import scala.util.{Success, Try}
+import scala.util.{Success, Failure, Try}
 
 object StringCalculator {
 
   val defaultDelimiter: Char = ','
   val cutoff = 1000
 
-  def add(numbers: String): Int =
+  def add(numbers: String): Try[Int] =
     numbers.toString.split(Array(delimiter(numbers),'\n'))
       .map(numberString => Try(numberString.toInt))
       .collect { case Success(n) => n }
       .filter(_ <= 1000)
+      .map {
+          case number if number >= 0 => Success(number)
+          case number if number < 0 => Failure(new IllegalArgumentException(number.toString))
+      }
       .sum
 
   private def delimiter(numbers: String): Char =
